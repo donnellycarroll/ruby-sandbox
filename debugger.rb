d07dea886ca5ae5263b7612fc65cbd482b6f960e
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-10-11 03:43:06 +0000

    debug practice

---
+++ debugger.rb
@@ -0,0 +1,7 @@
+require "pry-byebug"
+
+lucky_num = rand(1..20)
+
+pp "Your lucky numer is " + lucky_num + "!"
+
+byebug
---
+++ howdy.rb
@@ -0,0 +1,10 @@
+require "active_support/all"
+require "./goodbye.rb"
+
+
+#my_string = "Hello!"
+
+#pp my_string
+
+pp 1.ordinalize
+pp 324.ordinalize
